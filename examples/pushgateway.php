3798cae1a0f1aac3b5ab1101b9e3fe614da8bd44
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2020-09-21 07:11:24 +0200

    Initial commit

---
+++ examples/pushgateway.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Prometheus\CollectorRegistry;
+use Prometheus\PushGateway;
+use Prometheus\Storage\Redis;
+
+$adapter = $_GET['adapter'];
+
+if ($adapter === 'redis') {
+    Redis::setDefaultOptions(['host' => $_SERVER['REDIS_HOST'] ?? '127.0.0.1']);
+    $adapter = new Prometheus\Storage\Redis();
+} elseif ($adapter === 'apc') {
+    $adapter = new Prometheus\Storage\APC();
+} elseif ($adapter === 'in-memory') {
+    $adapter = new Prometheus\Storage\InMemory();
+}
+
+$registry = new CollectorRegistry($adapter);
+
+$counter = $registry->registerCounter('test', 'some_counter', 'it increases', ['type']);
+$counter->incBy(6, ['blue']);
+
+$pushGateway = new PushGateway('192.168.59.100:9091');
+$pushGateway->push($registry, 'my_job', ['instance' => 'foo']);
